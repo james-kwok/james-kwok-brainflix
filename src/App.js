906aa4bc93c01045404879119d44062aa7d6c151
james kwok <kinkwanjames@gmail.com>
2022-11-01 22:47:42 -0400

    create nav component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import Navigation from './component/Component/Navigation.jsx';
 import './App.scss';
 
-
+function App() {
+    return (
+    <>
+        <Navigation />
+    </>
+    );
+}
 export default App;
