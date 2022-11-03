8b5dcdaaa0eec36baf3d625f93b75a030f43c284
james kwok <kinkwanjames@gmail.com>
2022-11-03 19:19:43 -0400

    create video details component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navigation from './component/Navigation/Navigation.jsx';
 import Hero from './component/Hero/Hero.jsx';
+import VideoDetails from './component/VideoDetails/VideoDetails.jsx';
+import displayDetails from './data/video-details.json';
+import { useState } from "react";
 import './App.scss';
 
 function App() {
+
+    const [ displayVideo, setDisplayVideo] = useState(displayDetails[0])
+
     return (
     <>
         <Navigation />
         <Hero />
+        <VideoDetails displayVideo = {displayVideo}/>
     </>
     );
 }
